7bd752ec184c28862a4c8f25d38b4099491271c2
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-06-15 15:05:09 +0530

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const crypto = require("crypto");
+
+// 3072 Modp Group
+const alice = crypto.getDiffieHellman("modp15");
+const bob = crypto.getDiffieHellman("modp15");
+
+// Generating Keys
+alice.generateKeys();
+bob.generateKeys();
+
+// Public Keys
+alicePublicKey = alice.getPublicKey();
+bobPublicKey = bob.getPublicKey();
+
+const aliceSecret = alice.computeSecret(bobPublicKey, null, "hex");
+const bobSecret = bob.computeSecret(alicePublicKey, null, "hex");
+
+console.log(aliceSecret === bobSecret);
