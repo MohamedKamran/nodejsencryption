b4874f4a47529c4922090021d55fab369638aa3a
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-06-15 15:14:29 +0530

    made new means

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const crypto = require("crypto");
+const testDeffieHeilman = require("./deffie-heilman");
 
-// 3072 Modp Group
-const alice = crypto.getDiffieHellman("modp15");
-const bob = crypto.getDiffieHellman("modp15");
-
-// Generating Keys
-alice.generateKeys();
-bob.generateKeys();
-
-// Public Keys
-alicePublicKey = alice.getPublicKey();
-bobPublicKey = bob.getPublicKey();
-
-const aliceSecret = alice.computeSecret(bobPublicKey, null, "hex");
-const bobSecret = bob.computeSecret(alicePublicKey, null, "hex");
-
-console.log(aliceSecret === bobSecret);
+testDeffieHeilman();
